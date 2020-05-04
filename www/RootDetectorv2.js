cd6c0550960aba04273ddfc8b9e40abb0b890b55
fnazal <fnazal.3it@bancochile.cl>
2020-05-03 20:55:30 -0400

    args

--- www/RootDetectorv2.js
+++ www/RootDetectorv2.js
@@ -3,8 +3,8 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'RootDetectorv2';
 
 var rootdetector = {
-    listApps: function (arg0, success, error) {
-        exec(success, error, PLUGIN_NAME, 'listApps', [arg0]);
+    listApps: function (success, error) {
+        exec(success, error, PLUGIN_NAME, 'listApps', []);
     }
 };
 
