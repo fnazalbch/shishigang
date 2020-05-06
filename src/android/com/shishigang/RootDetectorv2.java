652f3f362d9e1ff430f1454967f8c2be83132493
fnazal <fnazal.3it@bancochile.cl>
2020-05-06 16:39:49 -0400

    Fix

--- src/android/com/shishigang/RootDetectorv2.java
+++ src/android/com/shishigang/RootDetectorv2.java
@@ -30,6 +30,8 @@ public class RootDetectorv2 extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        JSONObject result = new JSONObject();
+
         if (action.equals("listApps")) {
             PackageManager packageManager = this.cordova.getActivity().getPackageManager();
             List<ApplicationInfo> packages = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
@@ -58,7 +60,7 @@ public class RootDetectorv2 extends CordovaPlugin {
             return false;
         }
         else if (action.equals("isSELinux")) {
-            boolean result = isSELinuxEnforced();
+            isSELinuxEnforced(result);
             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
             return true;
         }
@@ -68,7 +70,7 @@ public class RootDetectorv2 extends CordovaPlugin {
         }
     }
 
-    private static boolean isSELinuxEnforced() {
+    private static boolean isSELinuxEnforced(JSONObject result) {
         StringBuffer output = new StringBuffer();
         Process p;
 
@@ -82,19 +84,23 @@ public class RootDetectorv2 extends CordovaPlugin {
             }
         }
         catch (Exception e) {
+            result.put("Catch", false);
             Log.e("Ionic Android Plugin", "No existe soporte getenforce");
             e.printStackTrace();
             return false;
         }
         String response = output.toString();
         if ("Enforcing".equals(response)) {
+            result.put("Result", true)
             return true;
         }
         else if ("Permissive".equals(response)) {
+            result.put("Permissive", false);
             return false;
         }
         else {
             Log.e("Ionic Android Plugin", "Valor inesperado");
+            result.put("Unexpected", false);
             return false;
         }
     }
