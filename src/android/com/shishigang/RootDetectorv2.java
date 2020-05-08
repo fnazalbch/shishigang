d241afaf37d7f4452c8ca827dd95e597798cebf1
fnazal <fnazal.3it@bancochile.cl>
2020-05-08 00:26:15 -0400

    Fix

--- src/android/com/shishigang/RootDetectorv2.java
+++ src/android/com/shishigang/RootDetectorv2.java
@@ -25,6 +25,68 @@ import com.shishigang.RootUtils;
  */
 public class RootDetectorv2 extends CordovaPlugin {
 
+    public static final String[] knownRootAppsPackages = {
+            "com.noshufou.android.su",
+            "com.noshufou.android.su.elite",
+            "eu.chainfire.supersu",
+            "com.koushikdutta.superuser",
+            "com.thirdparty.superuser",
+            "com.yellowes.su",
+            "com.zachspong.temprootremovejb",
+            "com.ramdroid.appquarantine",
+            "eu.chainfire.supersu",
+            "com.topjohnwu.magisk"
+    };
+
+    public static final String[] knownDangerousAppsPackages = {
+            "com.koushikdutta.rommanager",
+            "com.koushikdutta.rommanager.license",
+            "com.dimonvideo.luckypatcher",
+            "com.chelpus.lackypatch",
+            "com.ramdroid.appquarantine",
+            "com.ramdroid.appquarantinepro"
+    };
+
+    /** EdXposed */
+    public static final String[] knownRootCloakingPackages = {
+            "com.devadvance.rootcloak",
+            "com.devadvance.rootcloakplus",
+            "de.robv.android.xposed.installer",
+            "com.saurik.substrate",
+            "com.zachspong.temprootremovejb",
+            "com.amphoras.hidemyroot",
+            "com.amphoras.hidemyrootadfree",
+            "com.formyhm.hiderootPremium",
+            "com.formyhm.hideroot"
+    };
+
+    public static final String[] suPaths ={
+            "/data/local/",
+            "/data/local/bin/",
+            "/data/local/xbin/",
+            "/sbin/",
+            "/su/bin/",
+            "/system/bin/",
+            "/system/bin/.ext/",
+            "/system/bin/failsafe/",
+            "/system/sd/xbin/",
+            "/system/usr/we-need-root/",
+            "/system/xbin/"
+    };
+
+    public static final String[] pathsThatShouldNotBeWritable = {
+            "/system",
+            "/system/bin",
+            "/system/sbin",
+            "/system/xbin",
+            "/vendor/bin",
+            "/sys",
+            "/sbin",
+            "/etc",
+            "/proc",
+            "/dev"
+    };
+
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -57,7 +119,7 @@ public class RootDetectorv2 extends CordovaPlugin {
                     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Se ha encontrado app root"));
                     return true;
                 } */
-                if (RootUtils.checkForRootPackages(packageInfo.packageName)) {
+                if (checkForRootPackages(packageInfo.packageName)) {
                     callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, "Se ha encontrado app root"));
                     return true;
                 }
@@ -113,6 +175,24 @@ public class RootDetectorv2 extends CordovaPlugin {
             result.put("Unexpected", false);
             return false;
         }
+    }
+
+    public static boolean checkForRootPackages(String userPackage) {
+        for (String pkg : knownRootAppsPackages) {
+            if (userPackage.equals(pkg)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkForDangerousApps(String userPackage) {
+        for (String pkg : knownDangerousAppsPackages) {
+            if (userPackage.equals(pkg)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
